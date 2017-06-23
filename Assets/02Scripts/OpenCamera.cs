4fe21eb5cb66c7f44ab47a4b35eea489e00c7d09
Xiong <yuan.xiong@intel.com>
2017-06-23 14:55:45 +0800

    First commit: use webcam in unity.

---
+++ Assets/02Scripts/OpenCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenCamera : MonoBehaviour {
+
+	public RawImage image;
+	public WebCamTexture webTex;
+	public string deviceName;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Start");
+
+		StartCoroutine ("CallCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("Update");
+
+		if(webTex != null) {
+			image.texture = webTex;
+		}
+	}
+
+	private IEnumerator CallCamera() {
+		Debug.Log ("CallCamera");
+
+		yield return Application.RequestUserAuthorization (UserAuthorization.WebCam);
+		if (Application.HasUserAuthorization(UserAuthorization.WebCam)) {
+			WebCamDevice[] devices = WebCamTexture.devices;
+			deviceName = devices [0].name;
+			webTex = new WebCamTexture (deviceName, Screen.width, Screen.height, 20);
+			webTex.Play ();
+		}
+	}
+}
